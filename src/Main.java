86dee60f08f7c2e9840169d7e2cba5c16b8bed5d
AndresGalvan <agalvancerdeiras@gmail.com>
2022-06-11 13:44:58 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Cliente cliente = new Cliente();
+        Trabajador trabajador = new Trabajador();
+
+        cliente.edad = 22;
+        cliente.nombre = "Pepe";
+        cliente.telefono= 664896541;
+        cliente.credito= 3000;
+
+        System.out.println("la edad del cliente es de " + cliente.edad  + " años");
+        System.out.println("el cliente se llama " + cliente.nombre);
+        System.out.println("el télefono del cliente es el " + cliente.telefono);
+        System.out.println("el credito del cliente es de " + cliente.credito + " Euros");
+
+        trabajador.edad = 24;
+        trabajador.nombre = "Juan";
+        trabajador.telefono = 667458521;
+        trabajador.salario = 1300;
+
+        System.out.println("la edad del trabajador es de " + trabajador.edad  + " años");
+        System.out.println("el trabajador se llama " + trabajador.nombre);
+        System.out.println("el télefono del trabajador es el " + trabajador.telefono);
+        System.out.println("el trabajador tiene un salario de " + trabajador.salario + " Euros");
+
     }
+}
+
+class Persona {
+    int edad;
+    String nombre;
+    int telefono;
+}
+
+class Cliente extends Persona{
+    int credito;
+}
+
+class Trabajador extends Persona{
+    int salario;
 }
